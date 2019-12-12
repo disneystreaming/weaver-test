3ecd0d8240c25c5bcf5b129e1cb8b14cd4a74e65
Olivier Mélois <olivier.melois@disneystreaming.com>
2019-12-12 16:23:22 +0100

    ISSUE-15 Report object initalisation failure
    When a suite fails to be loaded (because of a thrown exception
    at the top level of the object for instance), we now reports it to the
    user rather than silencing it.

--- modules/framework/src/weaver/framework/Task.scala
+++ modules/framework/src/weaver/framework/Task.scala
@@ -10,8 +10,8 @@ import sbt.testing.{ Logger => BaseLogger, Task => BaseTask, _ }
 
 import scala.concurrent.duration._
 import scala.concurrent.{ Await, Promise }
-import scala.util.Try
 import scala.util.control.NonFatal
+import scala.util.control.NoStackTrace
 
 final class Task(
     val task: TaskDef,
@@ -33,55 +33,53 @@ final class Task(
     def handle(event: TestOutcome): IO[Unit] =
       IO(eventHandler.handle(sbtEvent(event)))
 
+    def doLog(event: TestOutcome): IO[Unit] =
+      loggers.toVector.foldMap(logger => IO(logger.info(event.formatted)))
+
     val defaultLoggedBracket: LoggedBracket = withLogger =>
-      withLogger((_, event) =>
-        loggers.toList.traverse(doLog(event)) *> handle(event) *> IO.unit)
+      withLogger((_, event) => doLog(event) *> handle(event))
 
     val loggedBracket = maybeLoggedBracket.getOrElse(defaultLoggedBracket)
 
     loggedBracket
-      .apply { log =>
+      .apply { deferredLog =>
         // format: off
         val reportSink: fs2.Pipe[IO, TestOutcome, Unit] = _.flatMap[IO, Unit] {
           case event @ TestOutcome(_, _, Result.Success | Result.Ignored(_, _) | Result.Cancelled(_, _), _) =>
-            fs2.Stream.eval((loggers.toList.traverse(doLog(event)) *> handle(event) *> IO.unit))
+            fs2.Stream.eval(doLog(event) *> handle(event))
           case event =>
-            fs2.Stream.eval(handle(event) *> log(task.fullyQualifiedName(), event))
+            fs2.Stream.eval(handle(event) *> deferredLog(task.fullyQualifiedName(), event))
         }
         // format: on
 
-        loadSuite(task.fullyQualifiedName(), cl).fold(IO.unit) { suite =>
-          loggers.foreach(_.info(cyan(task.fullyQualifiedName())))
-
-          suite
-            .ioSpec(args)
-            .through(reportSink)
-            .compile
-            .drain
-            .map(_ => loggers.foreach(_.info(EOL)))
-            .handleErrorWith {
-              case NonFatal(e) => // Unexpected failure
-                val event: TestOutcome =
-                  TestOutcome("Unexpected error",
-                              0.seconds,
-                              Result.from(e),
-                              Chain.empty)
-                for {
-                  _ <- reportError(eventHandler, e)
-                  _ <- log(task.fullyQualifiedName(), event)
-                } yield ()
-            }
-        }
+        loadSuite(task.fullyQualifiedName(), cl)
+          .flatMap { suite =>
+            loggers.foreach(_.info(cyan(task.fullyQualifiedName())))
+            suite
+              .ioSpec(args)
+              .through(reportSink)
+              .compile
+              .drain
+              .map(_ => loggers.foreach(_.info(EOL)))
+          }
+          .handleErrorWith {
+            case NonFatal(e) =>
+              val event: TestOutcome =
+                TestOutcome("Unexpected failure",
+                            0.seconds,
+                            Result.from(e),
+                            Chain.empty)
+              for {
+                _ <- reportError(eventHandler, e)
+                _ <- deferredLog(task.fullyQualifiedName(), event)
+              } yield ()
+          }
       }
       .flatMap(_ => maybeNext)
       .unsafeRunAsync {
         case Right(nextTask) => continuation(nextTask.toArray)
         case Left(_)         => continuation(Array())
       }
-  }
-
-  def doLog(event: TestOutcome)(logger: BaseLogger): IO[Unit] = {
-    IO(logger.info(event.formatted))
   }
 
   def reportError(eventHandler: EventHandler, t: Throwable): IO[Unit] = IO {
@@ -104,9 +102,13 @@ final class Task(
     Await.result(p.future, Duration.Inf)
   }
 
-  def loadSuite(name: String, loader: ClassLoader): Option[EffectSuite[Any]] = {
-    Try(TestUtils.loadModule(name, loader)).toOption
-      .collect { case ref: EffectSuite[_] => ref }
-  }
+  def loadSuite(name: String, loader: ClassLoader): IO[EffectSuite[Any]] =
+    IO(TestUtils.loadModule(name, loader)).flatMap {
+      case ref: EffectSuite[_] => IO.pure(ref)
+      case other =>
+        IO.raiseError {
+          new Exception(s"$other is not an effect suite") with NoStackTrace
+        }
+    }
 
 }
