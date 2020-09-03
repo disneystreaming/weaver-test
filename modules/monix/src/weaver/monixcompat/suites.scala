59a29ac415928b0991a1d31c48230281978dfbec
Bruno Fernandes <bfdes@users.noreply.github.com>
2020-09-03 23:00:25 +0100

    scalafmt

--- modules/monix/src/weaver/monixcompat/suites.scala
+++ modules/monix/src/weaver/monixcompat/suites.scala
@@ -8,12 +8,12 @@ import monix.eval.instances._
 import monix.execution.Scheduler
 import cats.effect.{ Timer, ContextShift, ConcurrentEffect }
 
-trait BaseMonixSuite { self : ConcurrentEffectSuite[Task] =>
-  val ec = scala.concurrent.ExecutionContext.global
-  val scheduler = Scheduler(ec)
-  implicit def timer : Timer[Task] = Task.timer(scheduler)
-  implicit def cs : ContextShift[Task] = Task.contextShift(scheduler)
-  implicit def effect : ConcurrentEffect[Task] =
+trait BaseMonixSuite { self: ConcurrentEffectSuite[Task] =>
+  val ec                              = scala.concurrent.ExecutionContext.global
+  val scheduler                       = Scheduler(ec)
+  implicit def timer: Timer[Task]     = Task.timer(scheduler)
+  implicit def cs: ContextShift[Task] = Task.contextShift(scheduler)
+  implicit def effect: ConcurrentEffect[Task] =
     new CatsConcurrentEffectForTask()(scheduler, Task.defaultOptions)
 }
 
