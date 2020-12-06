f4667464cb78ebabd428d36419e0dbc57d7fb823
Olivier Melois <olivier.melois@disneystreaming.com>
2020-12-06 22:53:59 +0100

    Aligned MonixBIO cancel token on the others

--- modules/core/monixBio/src/weaver/monixbiocompat/MonixBioUnsafeRun.scala
+++ modules/core/monixBio/src/weaver/monixbiocompat/MonixBioUnsafeRun.scala
@@ -5,10 +5,10 @@ import cats.Parallel
 import cats.effect.{ ContextShift, Timer }
 
 import monix.bio.IO
-import monix.execution.Scheduler
+import monix.execution.{ Cancelable, Scheduler }
 
 object MonixBIOUnsafeRun extends UnsafeRun[monix.bio.Task] {
-  type CancelToken = monix.bio.Task[Unit]
+  type CancelToken = Cancelable
   implicit val scheduler: Scheduler = monix.execution.Scheduler.global
 
   implicit val effect                             = IO.catsEffect(scheduler)
@@ -16,11 +16,10 @@ object MonixBIOUnsafeRun extends UnsafeRun[monix.bio.Task] {
   implicit val contextShift: ContextShift[monix.bio.Task] =
     IO.contextShift(scheduler)
   implicit val timer: Timer[monix.bio.Task] = IO.timer(scheduler)
-  def background(task: monix.bio.Task[Unit]): monix.bio.Task[Unit] = {
-    val cancelToken = task.runAsync { _ => () }(scheduler)
-    monix.bio.Task(cancelToken.cancel())
+  def background(task: monix.bio.Task[Unit]): CancelToken = {
+    task.runAsync { _ => () }(scheduler)
   }
   def sync(task: monix.bio.Task[Unit]): Unit  = PlatformCompat.runSync(task)
   def async(task: monix.bio.Task[Unit]): Unit = task.runAsyncAndForget
-  def cancel(token: CancelToken): Unit        = sync(token)
+  def cancel(token: CancelToken): Unit        = token.cancel()
 }
