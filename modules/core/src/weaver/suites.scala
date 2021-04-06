2f0a7b7672998d2b468f158e0e47683edd25a565
Olivier Melois <olivier.melois@disneystreaming.com>
2021-04-06 11:36:30 +0200

    Remove unused class

--- modules/core/src/weaver/suites.scala
+++ modules/core/src/weaver/suites.scala
@@ -49,10 +49,6 @@ trait EffectSuite[F[_]] extends Suite[F] with EffectSuiteAux with SourceLocation
     spec(args).evalMap(report).compile.drain.adaptErr(adaptRunError)
 }
 
-sealed trait TestEvent
-case class TestStarted(testName: TestName) extends TestEvent
-case class TestEnded(outcome: TestOutcome) extends TestEvent
-
 @RunWith(classOf[weaver.junit.WeaverRunner])
 abstract class RunnableSuite[F[_]] extends EffectSuite[F] {
   implicit protected def effectCompat: UnsafeRun[EffectType]
