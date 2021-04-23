e4a0dce4866765ec7160670da7c4aa2f038338a6
Dario Abdulrehman <167463+dabd@users.noreply.github.com>
2021-04-23 22:26:48 +0100

    Add support for overriding CheckConfig in forall (#278)
    Addresses https://github.com/disneystreaming/weaver-test/issues/265
    
    - Fix small bug in stopping condition (https://github.com/disneystreaming/weaver-test/commit/7699d9c134bb02d41399345908231a051d688f12)
    - Add support for overriding `CheckConfig` in `forall`
    - Update README

--- modules/scalacheck/test/src/weaver/scalacheck/PropertyDogFoodTest.scala
+++ modules/scalacheck/test/src/weaver/scalacheck/PropertyDogFoodTest.scala
@@ -26,14 +26,20 @@ object PropertyDogFoodTest extends IOSuite {
         // Go into software engineering they say
         // Learn how to make amazing algorithms
         // Build robust and deterministic software
-        val (attempt, value) =
-          if (ScalaCompat.isScala3)
-            ("4", "-2147483648")
-          else
-            ("2", "0")
+        val (attempt, value, seed) =
+          if (ScalaCompat.isScala3) {
+            ("4",
+             "-2147483648",
+             """Seed.fromBase64("AkTFK0oQzv-BOkf-rqnsdb_Etapzkj9gQD9rHj7UnKM=")""")
+          } else {
+            ("2",
+             "0",
+             """Seed.fromBase64("Nj62qCHF96VYEMGcD2OBlfmuyihbPQQhQLH9acYL5RA=")""")
+          }
 
         val expectedMessage =
-          s"Property test failed on try $attempt with input $value"
+          s"Property test failed on try $attempt with seed $seed and input $value"
+
         expect(log.contains(expectedMessage))
       }
     }
@@ -49,20 +55,36 @@ object PropertyDogFoodTest extends IOSuite {
     }
   }
 
+  // 5 checks with perPropertyParallelism = 1 sleeping 1 second each should take at least 5 seconds
+  test("Config can be overridden") { dogfood =>
+    for {
+      events <- dogfood.runSuite(Meta.ConfigOverrideChecks).map(_._2)
+      _      <- expect(events.size == 1).failFast
+    } yield {
+      expect(events.headOption.get.duration() >= 5000)
+    }
+  }
 }
 
 object Meta {
-  object ParallelChecks extends SimpleIOSuite with Checkers {
+  trait ParallelChecks extends SimpleIOSuite with Checkers {
+
+    def partiallyAppliedForall: PartiallyAppliedForall
+
+    test("sleeping forall") {
+      partiallyAppliedForall { (x: Int, y: Int) =>
+        IO.sleep(1.second) *> IO(expect(x + y == y + x))
+      }
+    }
+  }
+
+  object ParallelChecks extends ParallelChecks {
+
+    override def partiallyAppliedForall: PartiallyAppliedForall = forall
 
     override def checkConfig: CheckConfig =
       super.checkConfig
         .copy(perPropertyParallelism = 100, minimumSuccessful = 100)
-
-    test("sleeping forall") {
-      forall { (x: Int, y: Int) =>
-        IO.sleep(1.second) *> IO(expect(x + y == y + x))
-      }
-    }
   }
 
   object FailedChecks extends SimpleIOSuite with Checkers {
@@ -75,5 +97,13 @@ object Meta {
         expect(x > 0)
       }
     }
+  }
+
+  object ConfigOverrideChecks extends ParallelChecks {
+
+    override def partiallyAppliedForall: PartiallyAppliedForall =
+      forall.withConfig(super.checkConfig.copy(
+        perPropertyParallelism = 1,
+        minimumSuccessful = 5))
   }
 }
