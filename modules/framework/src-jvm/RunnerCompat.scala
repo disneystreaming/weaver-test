33738c2f2483302a62a5007261e7664f4f474cae
Olivier Melois <olivier.melois@disneystreaming.com>
2021-04-23 12:30:48 +0200

    Increase gate timeout again

--- modules/framework/src-jvm/RunnerCompat.scala
+++ modules/framework/src-jvm/RunnerCompat.scala
@@ -111,7 +111,7 @@ trait RunnerCompat[F[_]] { self: sbt.testing.Runner =>
     stillRunning.set(sbtTasks.size)
 
     // Waiting for the resources to be allocated.
-    scala.concurrent.Await.result(gate.future, 120.second)
+    scala.concurrent.Await.result(gate.future, 300.second)
     sbtTasks.toArray
   }
 
