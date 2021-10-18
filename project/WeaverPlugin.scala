c3e50b5b51284ae0c8ae2469dcd9021108d1ce30
Scala Steward <me@scala-steward.org>
2021-10-18 14:13:14 +0200

    Update scala3-library, ... to 3.1.0

--- project/WeaverPlugin.scala
+++ project/WeaverPlugin.scala
@@ -150,7 +150,7 @@ object WeaverPlugin extends AutoPlugin {
 
   lazy val scala212               = "2.12.14"
   lazy val scala213               = "2.13.6"
-  lazy val scala3                 = "3.0.2"
+  lazy val scala3                 = "3.1.0"
   lazy val supportedScalaVersions = List(scala212, scala213, scala3)
 
   lazy val supportedScala2Versions = List(scala212, scala213)
