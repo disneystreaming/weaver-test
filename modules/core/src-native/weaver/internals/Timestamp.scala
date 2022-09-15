aa7296e2be5a9ba2cd6ccad7cda27c5b76c76b81
Olivier Mélois <olivier.melois@disneystreaming.com>
2022-09-15 12:48:58 +0200

    No patience on Native

--- modules/core/src-native/weaver/internals/Timestamp.scala
+++ modules/core/src-native/weaver/internals/Timestamp.scala
@@ -8,9 +8,9 @@ import posix.timeOps._
 
 private[weaver] object Timestamp {
 
-  def format(epochSecond: Long): String = Zone { implicit zone =>
-    val out     = alloc[time.tm]()
-    val timePtr = alloc[time.time_t]()
+  def format(epochSecond: Long): String = {
+    val out     = stackalloc[time.tm]()
+    val timePtr = stackalloc[time.time_t]()
     !timePtr = epochSecond
     val gmTime: Ptr[time.tm] = time.localtime_r(timePtr, out)
     val hour                 = gmTime.tm_hour
@@ -19,17 +19,16 @@ private[weaver] object Timestamp {
     s"$hour:$minutes:$seconds"
   }
 
-  def localTime(hours: Int, minutes: Int, seconds: Int): Long = Zone {
-    implicit zone =>
-      val out     = alloc[time.tm]()
-      val timePtr = alloc[time.time_t]()
-      !timePtr = time.time(null)
-      val gmTime: Ptr[time.tm] = time.gmtime_r(timePtr, out)
+  def localTime(hours: Int, minutes: Int, seconds: Int): Long = {
+    val out     = stackalloc[time.tm]()
+    val timePtr = stackalloc[time.time_t]()
+    !timePtr = time.time(null)
+    val gmTime: Ptr[time.tm] = time.gmtime_r(timePtr, out)
 
-      gmTime.tm_hour = hours
-      gmTime.tm_min = minutes
-      gmTime.tm_sec = seconds
-      gmTime.tm_isdst = -1; // Is DST on? 1 = yes, 0 = no, -1 = unknown
-      time.mktime(gmTime).longValue()
+    gmTime.tm_hour = hours
+    gmTime.tm_min = minutes
+    gmTime.tm_sec = seconds
+    gmTime.tm_isdst = -1; // Is DST on? 1 = yes, 0 = no, -1 = unknown
+    time.mktime(gmTime).longValue()
   }
 }
