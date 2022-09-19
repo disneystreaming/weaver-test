0dfcd95ff9cb03614c182afdd6c9ddd044643320
Scala Steward <scala_steward@virtuslab.com>
2022-09-19 15:40:10 +0000

    Update scala-library, scala-reflect to 2.12.17

--- project/WeaverPlugin.scala
+++ project/WeaverPlugin.scala
@@ -129,7 +129,7 @@ object WeaverPlugin extends AutoPlugin {
   override def requires = plugins.JvmPlugin
   override def trigger  = allRequirements
 
-  lazy val scala212               = "2.12.16"
+  lazy val scala212               = "2.12.17"
   lazy val scala213               = "2.13.8"
   lazy val scala3                 = "3.1.3"
   lazy val supportedScalaVersions = List(scala212, scala213, scala3)
