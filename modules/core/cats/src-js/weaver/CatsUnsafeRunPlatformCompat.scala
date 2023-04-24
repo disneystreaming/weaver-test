6e03a2cd53d4e542356ebd507b263380c34f2b3a
Olivier Mélois <olivier.melois@disneystreaming.com>
2023-04-24 13:32:34 +0200

    Attempt to have fatal errors lead to the test process ending

--- modules/core/cats/src-js/weaver/CatsUnsafeRunPlatformCompat.scala
+++ modules/core/cats/src-js/weaver/CatsUnsafeRunPlatformCompat.scala
@@ -1,12 +1,16 @@
 package weaver
 
 import cats.effect.IO
+import java.util.concurrent.atomic.AtomicBoolean
 
 private[weaver] trait CatsUnsafeRunPlatformCompat {
   self: CatsUnsafeRun =>
 
   def unsafeRunSync(task: IO[Unit]): Unit = ???
 
-  def background(task: IO[Unit]): CancelToken = ???
+  def background(task: IO[Unit], atomicBoolean: AtomicBoolean): CancelToken =
+    ???
+
+  def cancel(token: CancelToken): Unit = ???
 
 }
--- modules/core/cats/src/weaver/CatsUnsafeRun.scala
+++ modules/core/cats/src/weaver/CatsUnsafeRun.scala
@@ -3,18 +3,16 @@ package weaver
 import scala.concurrent.Future
 
 import cats.effect.unsafe.implicits.global
-import cats.effect.{ FiberIO, IO }
+import cats.effect.IO
 
 object CatsUnsafeRun extends CatsUnsafeRun
 
 trait CatsUnsafeRun extends UnsafeRun[IO] with CatsUnsafeRunPlatformCompat {
 
-  type CancelToken = FiberIO[Unit]
+  type CancelToken = () => Future[Unit]
 
   override implicit val parallel = IO.parallelForIO
   override implicit val effect   = IO.asyncForIO
-
-  def cancel(token: CancelToken): Unit = unsafeRunSync(token.cancel)
 
   def unsafeRunAndForget(task: IO[Unit]): Unit = task.unsafeRunAndForget()
   def unsafeRunToFuture(task: IO[Unit]): Future[Unit] = task.unsafeToFuture()
