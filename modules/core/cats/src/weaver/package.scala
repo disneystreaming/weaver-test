b59b94c91f3dc12b988b2ef6cfe17f96338d3c0f
Mark Canlas <github@htmlism.com>
2023-06-05 18:22:04 -0700

    add documentation to suite types

--- modules/core/cats/src/weaver/package.scala
+++ modules/core/cats/src/weaver/package.scala
@@ -2,10 +2,23 @@ import cats.effect.IO
 
 package object weaver {
 
-  type IOSuite        = MutableIOSuite
-  type SimpleIOSuite  = SimpleMutableIOSuite
+  /**
+   * Extend this when each test in the suite returns an `Resource[IO, Res]` for
+   * some shared resource `Res`
+   */
+  type IOSuite = MutableIOSuite
+
+  /**
+   * Extend this when each test in the suite returns an `IO[_]`
+   */
+  type SimpleIOSuite = SimpleMutableIOSuite
+
   type GlobalResource = IOGlobalResource
   type GlobalRead     = GlobalResourceF.Read[IO]
   type GlobalWrite    = GlobalResourceF.Write[IO]
-  type FunSuite       = FunSuiteIO
+
+  /**
+   * Extend this when each test in the suite is pure and does not return `IO[_]`
+   */
+  type FunSuite = FunSuiteIO
 }
