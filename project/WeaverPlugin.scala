e1faff4cea33aae2141201ae9a5a9af09e8aca29
Scala Steward <scala_steward@virtuslab.com>
2023-09-10 14:28:25 +0000

    Update scala3-library, ... to 3.3.1

--- project/WeaverPlugin.scala
+++ project/WeaverPlugin.scala
@@ -131,7 +131,7 @@ object WeaverPlugin extends AutoPlugin {
 
   lazy val scala212               = "2.12.17"
   lazy val scala213               = "2.13.10"
-  lazy val scala3                 = "3.2.2"
+  lazy val scala3                 = "3.3.1"
   lazy val supportedScalaVersions = List(scala212, scala213, scala3)
 
   lazy val supportedScala2Versions = List(scala212, scala213)
